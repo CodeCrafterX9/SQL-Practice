4b3bee7b1b82a00ccbbecc73b471a0d3315c7e5e
CodeCrafterX9 <prachiofficial14@gmail.com>
2025-12-28 13:40:22 +0530

    Create 1148. Article Views I.sql

---
+++ LeetCode SQL 50/Select/1148. Article Views I.sql
@@ -0,0 +1,82 @@
+/** 1148. Article Views I
+Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+Output: 
++------+
+| id   |
++------+
+| 4    |
+| 7    |
++------+
+**/
+with v as(
+select author_id,count(distinct article_id) as c
+from Views
+where author_id = viewer_id 
+group by author_id )
+
+select author_id id from v 
+where c>=1
+order by 1;
+
+/**
+Approach Used: with the v cte --> we count each author has watched how many unique " count(distinct article_id) " articles that they have written (where author_id = viewer_id )
+
+So for the given input table :
+Input: 
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+
+Result is :
+
+author_id | c
+   4      | 1  (article id 3)
+   7      | 1  (article id 2)
+
+after which i have written " select * from v " command --> where i fetch only those rows where author has watched more than 1 of its articles 
+(This could have been skipped..but ihvae written just in case the condition was >=2 or etc.)
+**/
