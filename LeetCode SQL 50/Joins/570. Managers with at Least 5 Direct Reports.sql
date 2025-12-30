735d386b00dd1138c7b71d0c6e6ce68a2c55c42f
CodeCrafterX9 <prachiofficial14@gmail.com>
2025-12-30 09:01:17 +0530

    Create 570. Managers with at Least 5 Direct Reports.sql

---
+++ LeetCode SQL 50/Joins/570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,59 @@
+/**
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+**/
+
+with m as(
+select managerId,count(*) reportee
+from Employee e
+group by managerId
+)
+select name
+from m join Employee e
+on m.managerId=e.id
+where m.reportee >=5 ;
+
+/** Approach: Basically first in M cte i have grouped the Employee table on managerId to find number of reportee under each manager
+Then I have joined the manager id with Employee table to find the names of those managers with atleast 5 employees under them**/
